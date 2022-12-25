6cf23c2e45b115a040f99eaf59f5a0c3c38ff5e7
K1LLsounD <sgol99@mail.ru>
2022-12-25 17:48:36 +0300

    add test in clean Selene and allure, allure results in gitignore

--- tests/test_clean_selene.py
+++ tests/test_clean_selene.py
@@ -1,18 +1,30 @@
-from selene import be, have
+import allure
+from allure_commons.types import Severity
+from selene import be
 from selene.support import by
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 
 browser.config.hold_browser_open = True
 
+@allure.tag('web')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'K1LLsounDD')
+@allure.link('https://github.com/', name='Url git')
+@allure.story('Tests on clean Selene')
+@allure.feature('Checking if a task is in the repository')
+def test_search_github():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.open('https://github.com/')
 
-def test_search_github_eroshenko_allure():
     s('[name="q"]').click()
-    s('[name="q"]').send_keys('eroshenkoam/allure-example')
+    s('[name="q"]').send_keys('K1LLsounDD/qa_guru_python_3_7_allure-reports')
     s('[name="q"]').submit()
 
-    s(by.link_text('eroshenkoam/allure-example')).click()
+    s(by.link_text('K1LLsounDD/qa_guru_python_3_7_allure-reports')).click()
 
     s('#issues-tab').click()
 
-    s(by.partial_text('#76')).should(be.visible)
+    s(by.partial_text('#1')).should(be.visible)
+    browser.quit()
---
+++ tests/test_with_steps.py
@@ -0,0 +1,33 @@
+import allure
+from allure_commons.types import Severity
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+@allure.tag('web')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'K1LLsounDD')
+@allure.link('https://github.com/')
+@allure.feature('Checking if a task is in the repository')
+@allure.story('Tests on pure Selene')
+def test_github_search():
+    with allure.step('Open github in a browser with a screen resolution of 1920x1080.'):
+        browser.config.window_width = 1920
+        browser.config.window_height = 1080
+        browser.open('https://github.com/')
+
+    with allure.step('Enter the name of the repository in the search'):
+        s('[name="q"]').click()
+        s('[name="q"]').send_keys('K1LLsounDD/qa_guru_python_3_7_allure-reports').press_enter()
+
+    with allure.step('Go to the repository itself'):
+        s(by.link_text('K1LLsounDD/qa_guru_python_3_7_allure-reports')).click()
+
+    with allure.step('Go to the issue tab'):
+        s('#issues-tab').click()
+
+    with allure.step('Search issue #1'):
+        s(by.partial_text('#1')).should(be.visible)
+
+    browser.quit()
